d8ba609f26dbf3989fecde781f63618385cf7e0b
Andrew <pereiraandrew557@gmail.com>
2026-03-12 09:52:58 -0400

    Kinda broken push, but want to be up to date with collaborators

---
+++ src/pages/admin/CreateEvent.tsx
@@ -0,0 +1,7 @@
+//let admin CRUD an event
+// can select from a list of existing Events to edit/delete
+// can make a new event, set its "type" and who is allowed to see it
+
+export default function CreateEvent(){
+    return <h1> hello </h1>
+}
--- src/pages/app/Scheduling.tsx
+++ src/pages/app/Scheduling.tsx
@@ -1,4 +1,6 @@
 export default function Scheduling() {
+  //list all days in a term, and then pull events and link them with their respective days and display appropriately. 
+  // events displayed will be decided by the user's "role", some events will only display for some roles. add filter feature 
   return (
     <section className="theme-card p-4 p-md-5">
       <h1 className="page-title">Scheduling</h1>
